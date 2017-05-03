97330e4e4fa522cbde0045f199f87aad808e949a
JakeMarshall <bojakemarshall43@gmail.com>
2017-05-04 01:14:16 +0800

    Update Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,80 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var autoprefixer = require('gulp-autoprefixer');
+// ------------------------------------------------
+// Plugins
+// ------------------------------------------------
 
+var gulp 			= require('gulp'),
+	sass 			= require('gulp-sass'),
+	browserSync 	= require('browser-sync').create(),
+	autoprefixer 	= require('gulp-autoprefixer'),
+	concat  		= require('gulp-concat'),
+	minify_css    	= require('gulp-minify-css'),
+	plumber 		= require('gulp-plumber'),
+	uglify 			= require('gulp-uglify'),
+	watch 			= require('gulp-watch');
+	sourcemaps    	= require('gulp-sourcemaps'),
+	notify 			= require('gulp-notify'),
+	imagemin 		= require('gulp-imagemin'),
+	pngquant 		= require('imagemin-pngquant');
+
+
+// ------------------------------------------------
+// Settings
+// ------------------------------------------------
+
+
+var dest_js 		= "dist/js";
+var dest_css 		= "dist/css";
+var src_sass 		= "src/scss/**/*.scss";
+var src_js 			= "src/js/**/*.js";
+
+
+
+
+
+
+
+// ------------------------------------------------
+
+
+// SASS to CSS
 
 gulp.task('styles', function(){
-	gulp.src('./scss/main.scss')
-	.pipe(sass())
-	.pipe(autoprefixer())
-	.pipe(gulp.dest('./css'))
-	.pipe(browserSync.reload({stream:true}));
+		
+		return gulp.src(src_sass)
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(autoprefixer('last 2 versions'))
+		.pipe(concat('app.min.css'))
+		.pipe(minify_css())
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(dest_css));
+		// .pipe(notify({ message: 'Hello world we are done' }));
 });
 
-gulp.task('serve', function() {
-	browserSync.init({
-		server:{
-			baseDir: './'
-		}
-	});
 
-	gulp.watch('./scss/*.scss', ['styles']);
-	gulp.watch('./**/*.html').on('change, browserSync.reload');
+
+// ------------------------------------------------
+
+
+// Compile JS
+
+gulp.task('js', function(){
+
+		return gulp(src_js)
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(concat('app.min.js'))
+		.pipe(gulp(dest_js));
 });
 
-	gulp.task('default', ['styles', 'serve']);
+
+
+
+
+
+
+
+
+
+
